8d780407c173d57e0df222468c65dfc4153129a2
Janeucode <enegbedeochuko@gmail.com>
2023-11-28 16:27:51 -0800

    add whitespace

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,5 +9,5 @@ if lastd > 5:
     print(f"Last digit of {number} is {lastd} and is greater than 5")
 if lastd == 0:
     print(f"Last digit of {number} is {lastd} and is 0")
-if lastd <6 and lastd != 0:
+if lastd < 6 and lastd != 0:
     print(f"Last digit of {number} is {lastd} and is less than 6 and not 0")
