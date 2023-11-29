30caeeb65871b53f85cc1cc505937de6ce071f9a
Janeucode <enegbedeochuko@gmail.com>
2023-11-28 17:44:30 -0800

    2nd try

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for number in range(99):
-    if number < 16 :
-        print(f"{number} = {hex(number)[:3]}\n", end='')
-    else:
-        print(f"{number} = {hex(number)[:4]}\n", end='')
+    print("{} = 0x{:x}".format(number, number))
