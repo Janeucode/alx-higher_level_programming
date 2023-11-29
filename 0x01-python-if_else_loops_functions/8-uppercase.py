47409017ad9b0d875c5d32d9a3dd482ce203cf7d
Janeucode <enegbedeochuko@gmail.com>
2023-11-29 15:54:46 -0800

    add format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,10 @@ def uppercase(s):
     for char in s:
         if 'a' <= char <= 'z':
             uppercase_char = chr(ord(char) - ord('a') + ord('A'))
-            print(uppercase_char, end='')
+            result += uppercase_char
         else:
-            print(char, end='')
+            result += char
+    print('{}'.format(result))
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = uppercase(str)
