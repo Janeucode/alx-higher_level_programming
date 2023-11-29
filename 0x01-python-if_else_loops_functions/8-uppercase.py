074e4902169d8301467d962df968e8a8b98aa440
Janeucode <enegbedeochuko@gmail.com>
2023-11-29 15:57:50 -0800

    initialize result

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(s):
     for char in s:
+        result = ''
         if 'a' <= char <= 'z':
             uppercase_char = chr(ord(char) - ord('a') + ord('A'))
             result += uppercase_char
