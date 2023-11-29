603eda63591ce6e3ca81e7ffabb489ba9e53a8b3
Janeucode <enegbedeochuko@gmail.com>
2023-11-28 18:45:09 -0800

    correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for tens_digit in range(10):
     for units_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, units_digit), end=', ' if units_digit < 9 else '\n')
+        print("{:02d}".format(10 * tens_digit + units_digit), end=', ' if units_digit < 9 else '\n')
