8315e54e85026c1eb254e503b3a6e09a6e26aeb4
Janeucode <enegbedeochuko@gmail.com>
2023-11-29 16:24:24 -0800

    correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-def uppercase(str):
+def uppercase(instr):
     result = ''
-    for char in str:
+    for char in instr:
         if 'a' <= char <= 'z':
             uppercase_char = chr(ord(char) - ord('a') + ord('A'))
             result += uppercase_char
         else:
             result += char
     print('{}'.format(result))
+    return result
 
 
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = uppercase(str)
-print(str)
+orstr = ''
+mstr = uppercase(orstr)
+print(mstr)
