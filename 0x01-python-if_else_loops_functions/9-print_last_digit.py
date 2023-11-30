96e591b22e5cc3fc65fde7f62fa902f3ba15b805
Janeucode <enegbedeochuko@gmail.com>
2023-11-29 16:50:01 -0800

    remix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     number = str(number)
-    if number:
-        last_d = number[-1]
-        print('{}'.format(last_d), end='')
-    else:
-        print('the input is empty')
+    last_d = number[-1]
+    print('{}'.format(last_d), end='')
