138ca3740b7de5ca918b49f9e02ed4e616d1f969
Janeucode <enegbedeochuko@gmail.com>
2023-11-29 18:25:04 -0800

    now correct by adding stops and spaces

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,13 @@
 def fizzbuzz():
     for n in range (1, 101):
         if n % 3 == 0:
-            print("Fizz")
+            print("Fizz", end=' ')
+            continue
         if n % 5 == 0:
-            print("Buzz")
+            print("Buzz", end=' ')
+            continue
         if n % 3 ==0 and n % 5 == 0:
-            print('FizzBuzz')
+            print('FizzBuzz', end=' ')
+            continue
         else:
             print('{}'.format(n), end=' ')
