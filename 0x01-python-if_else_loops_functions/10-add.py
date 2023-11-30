39e260fa88cc0f8010090210f0cea5915c7557cc
Janeucode <enegbedeochuko@gmail.com>
2023-11-29 17:46:14 -0800

    add two number

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def add(a, b):
+    addit = int(a) + int(b)
+    return addit
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    last_d = abs(number) % 10
+    print(last_d, end='')
+
     if number < 0:
-        number *= -1
+        print(last_d, end='')
 
     last_d = number % 10
-    print(last_d, end='')
+    return last_d
