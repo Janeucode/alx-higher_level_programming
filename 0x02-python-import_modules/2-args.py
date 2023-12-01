a6ff4596ae666a34360ea25dca2ca1f34fd93def
Janeucode <enegbedeochuko@gmail.com>
2023-11-30 18:19:11 -0800

    debug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,11 @@ from sys import argv
 if __name__ == "__main__":
     num_args = len(argv) - 1
 
-    if num_args == 0:
-        print("0 arguments.")
-    else:
-        print("{} argument{}".format(num_args, 's' if num_args > 1 else ''), end='')
-        print("." if num_args == 0 else "")
-        for i, arg in enumerate(argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+    print("{} argument{}{}".format(
+        num_args,
+        's' if num_args != 1 else '',
+        '' if num_args == 0 else ':'
+    ))
+
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, arg))
