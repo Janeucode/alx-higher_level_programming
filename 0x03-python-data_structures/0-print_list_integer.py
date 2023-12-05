017d2cf5292de646577d05499dc8d89ce237356b
Janeucode <enegbedeochuko@gmail.com>
2023-12-04 17:13:23 -0800

    set head to none

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
+def print_list_integer(my_list=None):
+    my_list = my_list or []
+
     for numbers in my_list:
         if isinstance(numbers, int) or isinstance(numbers, float):
             print("{}".format(numbers))
