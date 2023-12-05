cfb94fa6b5df3839dde567e582ffda76aa75bc7c
Janeucode <enegbedeochuko@gmail.com>
2023-12-04 17:11:25 -0800

    add decimals

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for numbers in my_list:
-        if isinstance(number, int) or isinstance(number, float):
+        if isinstance(numbers, int) or isinstance(numbers, float):
             print("{}".format(numbers))
